7d8035a740e0acd2b51e8871a1020c1ea23d8b69
kanikamittal06 <kanikamittal0606@gmail.com>
2017-02-18 15:38:01 +0530

    commit1

---
+++ breaktime.py
@@ -0,0 +1,10 @@
+import time
+import webbrowser
+totalBreak = 5
+start = 0
+while (start < totalBreak):
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=tnrXbHeuYiA&index=2&list=PLV8vIYTIdSna11Vc54-abg33JtVZiiMfg")
+    start += 1
+    
+    
